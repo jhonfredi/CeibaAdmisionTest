6cca7bd4991915482a0ef720f24c6d9c6fd87052
jhonfredi <jhonfredy-arias@hotmail.com>
2019-08-10 17:44:11 -0500

    Initial commit, Ceiba source, and some packages more

---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/contracts/IDataAccessManager.java
@@ -0,0 +1,6 @@
+package co.com.ceiba.mobile.pruebadeingreso.contracts;
+
+public interface IDataAccessManager {
+
+
+}
---
+++ app/src/main/java/co/com/ceiba/mobile/pruebadeingreso/dataAcces/entities/UserEntity.java
@@ -0,0 +1,4 @@
+package co.com.ceiba.mobile.pruebadeingreso.dataAcces.entities;
+
+public class UserEntity {
+}
